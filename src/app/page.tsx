c27b38c6332cafc268abb34da7ab3a88f6223d73
vickvey <vickvey0303@gmail.com>
2025-01-26 17:30:16 +0530

    Added Header component and updated some routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center p-24">Home Page</div>
-  )
+    <div className="flex flex-col items-center p-24 bg-blue-200 text-blue-800">
+      {/* Add button */}
+    </div>
+  );
 }
