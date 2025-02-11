b581617ab9d0fa93e29cf44c707971096fd76aa8
vickvey <vickvey0303@gmail.com>
2025-02-11 18:48:02 +0530

    deleted all components

--- src/app/how-to-play/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HowToPlayPage() {
-    return (
-        <div className="flex justify-center p-48 text-3xl bg-sky-200 text-blue-800">
-            <p>So basically Playing this game is very easy just tap the Play button.</p>
-        </div>
-    )
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,34 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Quiz App | NextJS",
-  description: "A simple quiz app made in NextJS",
-};
+  title: 'Quiz App | NextJS',
+  description: 'A simple quiz app made in NextJS',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
         {children}
       </body>
     </html>
-  );
+  )
 }
