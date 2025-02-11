52323aa7d4556ce29bff790cb98404af0b118f76
vickvey <vickvey0303@gmail.com>
2025-02-11 19:59:19 +0530

    Updated HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center p-24 bg-blue-200 text-blue-800">
-      {/* Add button */}
+    <div className='bg-gradient-to-br from-slate-700 via-gray-600 to-slate-800 h-screen w-screen flex flex-col justify-center items-center text-[#fff] gap-6'>
+      <h1 className='font-bold font-poppins text-6xl text-white/80'>
+        Quiz App in NextJS
+      </h1>
+      <p className='text-2xl text-white/50'>
+        Are you ready to test your mind?? Are you genius enough to tackle these
+        questions??
+      </p>
+      <div className='p-4 hover:bg-gray-700 hover:bg-transparent hover:rounded-xl hover:transition'>
+        <Link href={'/start-quiz'}>
+          <p className='italic text-xl font-monteserrat text-blue-100/60 hover:underline'>
+            Start the Quiz to find out
+          </p>
+        </Link>
+      </div>
     </div>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans'],
+        monteserrat: ['Monteserrat', 'sans'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
